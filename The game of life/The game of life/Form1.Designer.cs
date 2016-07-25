707b1d0a57883ce2d700c3ec7dd3b0bf15fe671d
Timo Borner <timo@borners.de>
2016-07-25 16:10:02 +0200

    Finished :)
    Wasn't such a difficult thing to program :) Although it's not a very
    effective and a pretty obvious algorithm... exe following

---
+++ The game of life/The game of life/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace The_game_of_life
+{
+    partial class gol
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Speed = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Speed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(13, 13);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start!";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 550;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Speed
+            // 
+            this.Speed.Location = new System.Drawing.Point(12, 55);
+            this.Speed.Name = "Speed";
+            this.Speed.Size = new System.Drawing.Size(74, 45);
+            this.Speed.TabIndex = 1;
+            this.Speed.Scroll += new System.EventHandler(this.Speed_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Speed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Size";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 106);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(74, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1492, 844);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Speed);
+            this.Controls.Add(this.Start);
+            this.DoubleBuffered = true;
+            this.Name = "gol";
+            this.Text = "The game of life";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.gol_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.gol_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.gol_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Speed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar Speed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ The game of life/The game of life/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_game_of_life
+{
+    public partial class gol : Form
+    {
+        int sizex = 0, sizey = 0;
+        bool[,] Feld;
+        int fac = 100;
+        public gol()
+        {
+            InitializeComponent();
+        }
+
+        private void gol_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < sizex; i+=fac)
+            {
+                e.Graphics.DrawLine(Pens.Black, 0, i, sizex, i);
+                e.Graphics.DrawLine(Pens.Black, i, 0, i, sizey);
+            }
+            for (int x = 0; x < Feld.GetLength(1); x++)
+            {
+                for (int y = 0; y < Feld.GetLength(0); y++)
+                {
+                    if (Feld[y, x])
+                        e.Graphics.FillRectangle(Brushes.Yellow, (float)(fac*x), (float)(fac*y), fac, fac);
+                }
+            }
+        }
+
+        private void gol_MouseDown(object sender, MouseEventArgs e)
+        {
+            Feld[e.Y / fac, e.X / fac] = !Feld[e.Y / fac, e.X / fac];
+            Refresh();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            if (Start.Text == "Start!") Start.Text = "End!";
+            else Start.Text = "Start!";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool[,] newone = new bool[Feld.GetLength(0), Feld.GetLength(1)];
+            for (int x = 0; x < Feld.GetLength(1); x++)
+            {
+                for (int y = 0; y < Feld.GetLength(0); y++)
+                {
+                    newone[y, x] = getneighbours(x, y, Feld) == 3 || getneighbours(x, y, Feld) == 2 && Feld[y, x];
+                }
+            }
+            Feld = newone;
+            Refresh();
+        }
+
+        int getneighbours (int x, int y, bool[,] Feld)
+        {
+            int n = 0;
+            if(x > 0)
+            {
+                if (Feld[y, x - 1]) n++;
+                if (y > 0 && Feld[y - 1, x - 1]) n++;
+                if (y < Feld.GetLength(0) - 1 && Feld[y + 1, x - 1]) n++;
+            }
+            if(x < Feld.GetLength(1) -1)
+            {
+                if (Feld[y, x + 1]) n++;
+                if (y > 0 && Feld[y - 1, x + 1]) n++;
+                if (y < Feld.GetLength(0) - 1 && Feld[y + 1, x + 1]) n++;
+            }
+            if (y > 0 && Feld[y - 1, x]) n++;
+            if (y < Feld.GetLength(0) - 1 && Feld[y + 1, x]) n++;
+            return n;
+        }
+
+        private void Speed_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = 550 - Speed.Value * 50;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            fac = 110 - 10 * trackBar1.Value;
+            bool[,] temp = clone(Feld);
+            gol_Load(new object(), new EventArgs());
+            involve(temp);
+            Refresh();
+        }
+
+        bool[,] clone (bool[,] a)
+        {
+            bool[,] b = new bool[a.GetLength(0), a.GetLength(1)];
+            for (int x = 0; x < a.GetLength(0); x++)
+            {
+                for (int y = 0; y < a.GetLength(1); y++)
+                {
+                    b[x, y] = a[x, y];
+                }
+            }
+            return b;
+        }
+
+        void involve(bool[,] a)
+        {
+            for (int y = 0; y < a.GetLength(0) && y < Feld.GetLength(0); y++)
+            {
+                for (int x = 0; x < a.GetLength(1) && x < Feld.GetLength(1); x++)
+                {
+                    Feld[y, x] = a[y, x];
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1_Tick(null, null);
+        }
+
+        private void gol_Load(object sender, EventArgs e)
+        {
+            sizex = gol.ActiveForm.Size.Width;
+            sizey = gol.ActiveForm.Size.Height;
+            Feld = new bool[sizey / fac, sizex / fac];
+        }
+    }
+}
